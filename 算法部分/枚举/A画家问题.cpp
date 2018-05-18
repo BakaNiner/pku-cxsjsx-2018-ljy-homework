45fcb13debd29342a578c12cf2eb0be64631c32a
BakaNiner <bakaniner@avdir.com>
2018-05-19 00:16:29 +0800

    fixed a bug though the problem had an AC

--- 算法部分/枚举/A画家问题.cpp
+++ 算法部分/枚举/A画家问题.cpp
@@ -33,14 +33,16 @@ int main() {
 	}
 	
 	int m = n * n;
-	int i = 1;
-	for(; i <= m; i++) {
+	int pos = m + 1;
+	for(int i = 1; i <= m; i++) {
 		if(A[i][i] == 0) {
 			int j = i + 1;
 			for(; j <= m && A[j][i] == 0; j++);
-			if(j == m + 1)
+			if(j == m + 1) {
+				pos = i;
 				// 因为系数矩阵是个带状矩阵，所以如果A[i][i]是0的话，右下角都是0，可以停止消元 
 				break;
+			}
 			for(int k = i; k <= m; k++)
 				swap(A[i][k], A[j][k]);
 			swap(b[i], b[j]);
@@ -53,10 +55,16 @@ int main() {
 			}
 	}
 	
-	// 判断是否有 0 = 1 这种方程 
+//	for(int i = 1; i <= m; i++) {
+//		for(int j = 1; j <= m; j++)
+//			printf("%d ", A[i][j]);
+//		printf("%d\n", b[i]);
+//	}
+	
+	// 判断是否有 0 = 1 这种方程，pos之后（包括pos）都是自由元 
 	int flag = 0;
-	for(int j = i; j <= m; j++)
-		if(b[j] != 0) {
+	for(int i = pos; i <= m; i++)
+		if(b[i] != 0) {
 			flag = 1;
 			break;
 		}
@@ -64,10 +72,29 @@ int main() {
 	if(flag)
 		printf("inf");
 	else {
-		// 不用枚举自由元，直接令所有自由元为0就是最小解 
-		for(int i = 1; i <= m; i++)
-			b[0] += b[i];
-		printf("%d", b[0]);
+		int ans = m;
+		// 自由元的个数
+		int s = m - pos + 1;
+		// 枚举自由元的取值
+		for(int i = (1 << s) - 1; ~i; i--) {
+			int x[maxn];
+			memset(x, 0, sizeof(x));
+			for(int j = pos; j <= m; j++)
+				x[j] = (i >> (j - pos)) & 1;
+			// 回代
+			for(int j = 1; j < pos; j++) {
+				x[j] = b[j];
+				for(int k = pos; k <= m; k++)
+					if(A[j][k] == 1)
+						x[j] ^= x[k];
+			}
+			// 统计 
+			int res = 0;
+			for(int j = 1; j <= m; j++)
+				res += x[j] == 1;
+			ans = min(ans, res);
+		}
+		printf("%d", ans);
 	}
 	return 0;
 }
--- 算法部分/枚举/C特殊密码锁.cpp
+++ 算法部分/枚举/C特殊密码锁.cpp
@@ -25,13 +25,15 @@ int main() {
 	for(int i = 1; i <= n; i++)
 		b[i] ^= str[i] - '0';
 	
-	int i = 1;
-	for(; i <= n; i++) {
+	int pos = n + 1;
+	for(int i = 1; i <= n; i++) {
 		if(A[i][i] == 0) {
 			int j = i + 1;
 			for(; j <= n && A[j][i] == 0; j++);
-			if(j == n + 1)
+			if(j == n + 1) {
+				pos = i;
 				break;
+			}
 			for(int k = i; k <= n; k++)
 				swap(A[i][k], A[j][k]);
 			swap(b[i], b[j]);
@@ -45,8 +47,8 @@ int main() {
 	}
 	
 	int flag = 0;
-	for(int j = i; j <= n; j++)
-		if(b[j] != 0) {
+	for(int i = pos; i <= n; i++)
+		if(b[i] != 0) {
 			flag = 1;
 			break;
 		}
@@ -54,9 +56,25 @@ int main() {
 	if(flag)
 		printf("impossible");
 	else {
-		for(int i = 1; i <= n; i++)
-			b[0] += b[i];
-		printf("%d", b[0]);
+		int ans = n;
+		int s = n - pos + 1;
+		for(int i = (1 << s) - 1; ~i; i--) {
+			int x[maxn];
+			memset(x, 0, sizeof(x));
+			for(int j = pos; j <= n; j++)
+				x[j] = (i >> (j - pos)) & 1;
+			for(int j = 1; j < pos; j++) {
+				x[j] = b[j];
+				for(int k = pos; k <= n; k++)
+					if(A[j][k] == 1)
+						x[j] ^= x[k];
+			}
+			int res = 0;
+			for(int j = 1; j <= n; j++)
+				res += x[j] == 1;
+			ans = min(ans, res);
+		}
+		printf("%d", ans);
 	}
 	return 0;
 }
